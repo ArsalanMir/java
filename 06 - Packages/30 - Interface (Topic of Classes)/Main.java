2b1e7f990cb632ba46d6bd3c87ae3d1e5d811dd9
ArsalanMir <97144693+ArsalanMir@users.noreply.github.com>
2024-10-03 21:51:00 +0530

    Add files via upload

---
+++ 06 - Packages/30 - Interface (Topic of Classes)/Main.java
@@ -0,0 +1,44 @@
+// Program to demonstrate interface in java
+
+interface InterfaceClass // interface is a collection of abstract methods and static constants
+{
+    void fn1();
+
+    void fn2();
+
+    void fn3();
+}
+
+class derived implements InterfaceClass
+{
+    public void fn1()
+    {
+        System.out.println("hi i am overridden fn1");
+    }
+
+    public void fn2()
+    {
+        System.out.println("hi i am overridden fn2");
+    }
+
+    public void fn3()
+    {
+        System.out.println("hi i am overridden fn3");
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+
+        derived d = new derived();
+        d.fn1();
+        d.fn2();
+        d.fn3();
+
+        // InterfaceClass i = new interfaceClass(); // interface cannot be instantiated
+
+        System.out.println("\nSyed Aarish Shah | CSE-20-50");
+    }
+}
